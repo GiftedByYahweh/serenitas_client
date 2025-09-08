b08a3d52bd3f027ff4169428ccfe1ae382e2d1d9
GiftedByYahweh <giftedbyyahweh@gmail.com>
2025-09-08 22:33:27 +0300

    init commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { NButton, NSpace, NDatePicker } from "naive-ui";
+</script>
+
+<template>
+  <n-space vertical>
+    <n-button type="primary">Primary Button</n-button>
+    <n-date-picker type="date" />
+  </n-space>
+</template>
